731bfb05f0ea8777d73d983b7a03da4573686039
liaaneeee <thaovanliane.nguyen@yahoo.de>
2023-05-30 13:55:30 +0200

    Combine all annotated data including new
    Combine all annotated data including new

--- annotation/join_annotated_samples.py
+++ annotation/join_annotated_samples.py
@@ -5,6 +5,11 @@ if __name__ == '__main__':
     for i in range(1, 11):
         df = pd.read_csv(f"annotated_data/sample{i}.csv", sep="\t")
         samples.append(df)
+    # Append the additional samples
+    for i in range(1, 11):
+        df = pd.read_csv(f"more_annotated_data/sample{i}.csv", sep="\t")
+        samples.append(df)
     data = pd.concat(samples, axis=0)
     data.drop("Unnamed: 0", axis=1, inplace=True)
-    data.to_csv("../data/combined_samples.csv", sep="\t")
+    # data.to_csv("../data/combined_samples.csv", sep="\t")
+    data.to_csv("../data/new_combined_samples.csv", sep="\t")
